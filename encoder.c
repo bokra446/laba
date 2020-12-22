6de0eac43eee011cd7869d2cf488b85f370d52f3
bokra <katja44667@gmail.com>
2020-12-22 13:11:11 +0300

    2 variety with guideline

--- encoder.c
+++ encoder.c
@@ -12,49 +12,49 @@ int main (const int argc, char* argv[]) {
     char str[80], key[80];
     unsigned int shift;
     if (argc == 4) {
-	    if (strcmp(argv[1],ConsoleAllowedFlags[0]) == 0){
+	    if (strcmp(argv[1], ConsoleAllowedFlags[0]) == 0) {
 		    mutableDecoderXor(argv[2], argv[3]);
 	    }
-	    else if (strcmp(argv[1],ConsoleAllowedFlags[1]) == 0) {
+	    else if (strcmp(argv[1], ConsoleAllowedFlags[1]) == 0) {
 		    mutableDecoderCaesar(argv[2], atoi(argv[3]));
 	    }
-      printf("%s\n", argv[2]);
+		printf("%s\n", argv[2]);
     }
-	  else if (argc == 1) {
-    printf("Выберите способ шифрования (xor или caesar)\n");
-    scanf("%s", flag);
-    if (!strcmp(allowedFlags[0], flag)) {
-        printf("Шифруемая строка: ");
-        scanf(" %99[^\n]", str);
-		if (!checkStrtoLetters (str)) {
-			return 0;
+	else if (argc == 1) {
+		printf("Выберите способ шифрования (xor или caesar)\n");
+		scanf("%s", flag);
+		if (!strcmp(allowedFlags[0], flag)) {
+			printf("Шифруемая строка: ");
+			scanf(" %99[^\n]", str);
+			if (!checkStrtoLetters (str)) {
+				return 0;
+			}
+			mutableStrip(str);
+			mutableToLower(str);
+			printf("Ключ: ");
+			scanf(" %99[^\n]", key);
+			mutableDecoderXor(str, key);
 		}
-        mutableStrip(str);
-        mutableToLower(str);
-        printf("Ключ: ");
-        scanf(" %99[^\n]", key);
-        mutableDecoderXor(str, key);
-    }
-    else if (!strcmp(allowedFlags[1], flag)) {
-        printf("Шифруемая строка: ");
-        scanf(" %99[^\n]", str);
-		if (!checkStrtoLetters (str)) {
-			return 0;
+		else if (!strcmp(allowedFlags[1], flag)) {
+			printf("Шифруемая строка: ");
+			scanf(" %99[^\n]", str);
+			if (!checkStrtoLetters (str)) {
+				return 0;
+			}
+			mutableStrip(str);
+			mutableToLower(str);
+			printf("Сдвиг: ");
+			scanf("%d", &shift);
+			mutableDecoderCaesar(str, shift);
 		}
-        mutableStrip(str);
-        mutableToLower(str);
-        printf("Сдвиг: ");
-        scanf("%d", &shift);
-        mutableDecoderCaesar(str, shift);
-    }
-    else {
-        printf("Неккоректные данные \n");
-        return 1;
-    }
-    puts(str);
+		else {
+			printf("Неккоректные данные \n");
+			return 1;
+		}
+		puts(str);
     }
     else{
-      printf("Неккоректные данные\n");
+		printf("Неккоректные данные\n");
     }
     return 0;
 }
